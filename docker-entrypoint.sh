7017bd9b168dd77eb67ad5f12f4e32ce446447bd
Kukielka <philipp_kutyla@gmx.de>
2018-06-26 12:48:05 +0200

    Fixed for newest Commit
    Substratum renamed a couple o' things, guess I have to too!

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,14 +4,14 @@ cd /git
 git clone https://github.com/SubstratumNetwork/SubstratumNode.git
 cd /git/SubstratumNode/node
 cargo build --release --verbose
-ln -sf /git/SubstratumNode/node/target/release/node /node/node
+ln -sf /git/SubstratumNode/node/target/release/SubstratumNode /node/SubstratumNode
 chmod -R 777 /node
 export RUST_BACKTRACE=1
 cp /tmp/resolv.conf /etc/resolv.conf
-screen -A -m -d -S substratum-node /node/node --dns_servers 1.1.1.1
-while [ ! -f /tmp/node.log ] ;
+screen -A -m -d -S substratum-node /node/SubstratumNode --dns_servers 1.1.1.1
+while [ ! -f /tmp/SubstratumNode.log ] ;
 do
       sleep 1
 done
 sleep 5
-tail -f /tmp/node.log
+tail -f /tmp/SubstratumNode.log
